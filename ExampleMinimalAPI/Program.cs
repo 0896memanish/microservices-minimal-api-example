37fe04e1378b95fc234d1a8bbce3255f925e713c
Manish Kumar Singh <0896memanish@gmail.com>
2025-02-15 16:05:53 +0530

    Completed Example API project that has no UI but only created minimal APIs for adding todo items.

--- ExampleMinimalAPI/Program.cs
+++ ExampleMinimalAPI/Program.cs
@@ -1,5 +1,42 @@
+using ExampleMinimalAPI;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
+app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) => await db.Todos.FindAsync(id));
+
+app.MapPost("/todoitems", async (TodoItem item, TodoDb db) =>
+{
+    db.Todos.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todoitems/{item.Id}", item);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, TodoItem input, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo == null) return Results.NotFound();
+    todo.Name = input.Name;
+    todo.IsComplete = input.IsComplete;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is TodoItem todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
 app.Run();
